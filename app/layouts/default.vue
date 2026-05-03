a1e1096a346fea05ef24979b4917ae44b04e792a
percussionfloh <florian.moldaschl@web.de>
2026-05-03 18:11:17 +0200

    add default.vue layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { de } from '@nuxt/ui/locale';
+
+useHead({
+    titleTemplate: (titleChunk) => {
+        return titleChunk ? `${titleChunk} | Musica Inaudita` : 'Musica Inaudita';
+    },
+});
+</script>
+
+<template>
+        <UApp :locale="de">
+            <div class="flex flex-col min-h-screen">
+                <TheHeader />
+                <TheNavigation />
+                <main class="py-8">
+                    <slot></slot>
+                </main>
+                <TheFooter class="mt-auto" />
+            </div>
+        </UApp>
+</template>
+
+<style>
+html {
+    font-size: 20px;
+}
+.bg-pattern {
+    background-color: oklch(51.868% 0.16644 296.494);
+}
+
+</style>
