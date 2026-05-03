25f579c2e9e59f6a6bbf8f432d8d5e602a498030
percussionfloh <florian.moldaschl@web.de>
2026-05-03 16:53:34 +0200

    npm install @nuxt/ui, supabase, i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+    compatibilityDate: '2025-07-15',
+    devtools: { enabled: false },
+      runtimeConfig: {
+        supabaseKey: process.env.SUPABASE_KEY,
+        zoteroApiKey: process.env.ZOTERO_API_KEY,
+        zoteroUserId: process.env.ZOTERO_USER_ID,
+        public: {
+            supabaseUrl: process.env.SUPABASE_URL
+        }
+    },
+    modules: ['@nuxt/content', '@nuxt/ui', '@nuxtjs/supabase', '@nuxtjs/i18n'],
+    css: ['~/assets/main.css'],
+    supabase: {
+        redirect: false,
+    }
+
+});
