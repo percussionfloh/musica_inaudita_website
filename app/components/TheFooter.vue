0ee50056d45233e1ca570eab83b0ff31154aaa42
percussionfloh <florian.moldaschl@web.de>
2026-05-04 13:25:20 +0200

    update Footer

--- app/components/TheFooter.vue
+++ app/components/TheFooter.vue
@@ -1,9 +1,38 @@
 <script setup>
+const localePath = useLocalePath();
+const { t } = useI18n();
 
+const columns = [
+    {
+        label: t('sitemap'),
+        children: [
+            {
+                label: t('legalNotice'),
+                to: localePath('legal-notice'),
+            },
+        ],
+    },
+];
 </script>
 
 <template>
-    <div>
-        FOOTER
-    </div>
+    <UFooter class="bg-pattern">
+        <template #top>
+            <UContainer>
+                <UFooterColumns :columns="columns" :ui="{
+                    list: 'grid grid-cols-2 gap-x-2 space-y-0 mt-3',
+                    link: 'text-white text-base',
+                    label: 'text-white text-lg',
+                }">
+                <template #right>
+                        <div>
+                            <div class="flex justify-end">
+                                <img src="../assets/mi.png" class="max-h-47.5" alt="Lückenfüller" />
+                            </div>
+                        </div>
+                    </template>
+                </UFooterColumns>
+            </UContainer>
+        </template>
+    </UFooter>
 </template>
