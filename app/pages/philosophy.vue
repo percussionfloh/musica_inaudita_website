eb4231a6fb3c3ae78aae273ec2ca8678b9d18c26
percussionfloh <florian.moldaschl@web.de>
2026-05-05 11:04:15 +0200

    add philosophy.vue

---
+++ app/pages/philosophy.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+    <UContainer class="grid gap-12 grid-rows-2 items-center">
+        <div class="grid grid-rows-2 items-center m-6">        <h1>Philosophie</h1>
+            <p>Musica inaudita ist eine studentisch organisierte Initiative an der Universität der Künste Berlin, deren Programme nur Musik enthalten, die von Künstler*innen geschrieben wurde, die auf Grund ihres Geschlechts, ihrer Religion, ihrer sozialen oder nationalen Herkunft, Hautfarbe, sexueller Identität, Sprache, Behinderung, politischer oder sonstiger Anschauung von der Geschichtsschreibung übergangen wurden.</p>
+        </div>
+    </UContainer>
+</template>
