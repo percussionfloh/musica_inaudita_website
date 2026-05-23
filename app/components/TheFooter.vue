b6c1c1e0059f5e22984befce54cb0fd3c19bccd0
percussionfloh <florian.moldaschl@web.de>
2026-05-23 19:36:42 +0200

    update TheFooter

--- app/components/TheFooter.vue
+++ app/components/TheFooter.vue
@@ -1,38 +1,15 @@
 <script setup>
-const localePath = useLocalePath();
-const { t } = useI18n();
 
-const columns = [
-    {
-        label: t('sitemap'),
-        children: [
-            {
-                label: t('legalNotice'),
-                to: localePath('legal-notice'),
-            },
-        ],
-    },
-];
 </script>
 
 <template>
-    <UFooter class="bg-pattern">
-        <template #top>
-            <UContainer>
-                <UFooterColumns :columns="columns" :ui="{
-                    list: 'grid grid-cols-2 gap-x-2 space-y-0 mt-3',
-                    link: 'text-white text-base',
-                    label: 'text-white text-lg',
-                }">
-                <template #right>
-                        <div>
-                            <div class="flex justify-end">
-                                <img src="../assets/mi.png" class="max-h-47.5" alt="Bild" />
-                            </div>
-                        </div>
-                    </template>
-                </UFooterColumns>
-            </UContainer>
-        </template>
-    </UFooter>
+  <UFooter class="bg-color: bg-fuchsia-300">
+    <template #left >
+        <img src="../assets/logo_transparent.png" alt="Logo" />
+    </template>
+
+    <template #right>
+        <p class="text-sm text-black">Copyright © {{ new Date().getFullYear() }}</p>
+    </template>
+  </UFooter>
 </template>
