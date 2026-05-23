ce67ea4a489e0323f4ddaddec0d69d5bcaa4c4c0
percussionfloh <florian.moldaschl@web.de>
2026-05-23 20:02:31 +0200

    update TheHeader

--- app/components/TheHeader.vue
+++ app/components/TheHeader.vue
@@ -1,32 +1,3 @@
-<!-- <script setup>
-const localePath = useLocalePath()
-</script>
-
-<template>
-    <header class="bg-primary-500 text-white bg-pattern">
-        <div class="flex justify-between items-center py-2 px-4 gap-4">
-            <div class="grid-cols-2 gap-4 w-full grid">
-                <div class="font-extrabold text-2xl">
-                    <NuxtLink :to="localePath('/')">
-                        Musica Inaudita
-                    </NuxtLink>
-                </div>
-                <div class="flex gap-2 justify-end items-center">
-                    <UButton
-                        icon="i-simple-icons-instagram"
-                        to="https://instagram.com/musica_inaudita"
-                        target="_blank"
-                        variant="ghost"
-                        color="neutral"
-                        square
-                        :ui="{ label: 'hidden', base: 'text-white hover:text-black' }"
-                    />
-                </div>
-            </div>
-        </div>
-    </header>
-</template> -->
-
 <script setup>
 
 const { t } = useI18n();
@@ -64,9 +35,11 @@ const items = [
 </script>
 
 <template>
-  <UHeader class="bg-color: bg-fuchsia-300" mode="slideover">
-    <template #title>
-      <p class="xl:text-4xl md:text-2xl"> Musica Inaudita </p>
+  <UHeader class="bg-color: bg-fuchsia-300 h-20 flex items-center" mode="slideover">
+    <template #title class="align-center">
+        <div class="flex items-center h-full">
+             <img src="../assets/logo_transparent.png" alt="Logo" class="h-40 w-auto block mt-4"/> <!-- muss noch dynamisch angepasst werden -->
+        </div>
     </template>
 
     <UNavigationMenu :items="items" />
